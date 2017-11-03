dc8317de963bc0b6dce1ef63f4ed94d6bb20f798
Huascar <huascarmc@hotmail.com>
2017-11-03 11:20:31 +0000

    added guitar construct

--- Raysmusicexchange/app/src/main/java/com/example/huascar/raysmusicexchange/Guitar.java
+++ Raysmusicexchange/app/src/main/java/com/example/huascar/raysmusicexchange/Guitar.java
@@ -4,6 +4,10 @@ package com.example.huascar.raysmusicexchange;
  * Created by huascar on 03/11/2017.
  */
 
-public class Guitar {
+public class Guitar implements Playable {
     private int NumberOfStrings;
+
+    public Guitar(int NumberOfStrings) {
+        this.NumberOfStrings = NumberOfStrings;
+    }
 }
