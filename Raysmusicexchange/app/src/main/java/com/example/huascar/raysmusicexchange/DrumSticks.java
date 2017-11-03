9bbb6806f34363bf20e92b4bd9d0bc391c5dc525
Huascar <huascarmc@hotmail.com>
2017-11-03 11:50:09 +0000

    DrumSticks are sellable

--- Raysmusicexchange/app/src/main/java/com/example/huascar/raysmusicexchange/DrumSticks.java
+++ Raysmusicexchange/app/src/main/java/com/example/huascar/raysmusicexchange/DrumSticks.java
@@ -4,8 +4,12 @@ package com.example.huascar.raysmusicexchange;
  * Created by huascar on 03/11/2017.
  */
 
-public class DrumSticks extends Item {
+public class DrumSticks extends Item implements Sellable {
     public DrumSticks(String Type, double buyPrice, double sellPrice) {
         super(Type, buyPrice, sellPrice);
+    }
+
+    public void Sell() {
+        
     }
 }
