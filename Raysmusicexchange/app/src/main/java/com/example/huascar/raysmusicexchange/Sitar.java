9066d04899ae6d296ed66de281d52c8dcebedb26
Huascar <huascarmc@hotmail.com>
2017-11-03 11:24:02 +0000

    play method for sitar

--- Raysmusicexchange/app/src/main/java/com/example/huascar/raysmusicexchange/Sitar.java
+++ Raysmusicexchange/app/src/main/java/com/example/huascar/raysmusicexchange/Sitar.java
@@ -4,10 +4,14 @@ package com.example.huascar.raysmusicexchange;
  * Created by huascar on 03/11/2017.
  */
 
-public class Sitar {
+public class Sitar implements Playable{
     private int NumberOfStrings;
 
     public Sitar(int NumberOfStrings) {
         this.NumberOfStrings = NumberOfStrings;
+    }
+
+    public String Play() {
+        return "Taaan Tiin Tooon Tutaataraaan!";
     }
 }
