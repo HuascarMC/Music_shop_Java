129057970fb602edb25eb91bf2bd3922c09db7d5
Huascar <huascarmc@hotmail.com>
2017-11-03 11:19:34 +0000

    play method for hangdrum

--- Raysmusicexchange/app/src/main/java/com/example/huascar/raysmusicexchange/HangDrum.java
+++ Raysmusicexchange/app/src/main/java/com/example/huascar/raysmusicexchange/HangDrum.java
@@ -4,10 +4,14 @@ package com.example.huascar.raysmusicexchange;
  * Created by huascar on 03/11/2017.
  */
 
-public class HangDrum {
+public class HangDrum implements Playable {
     private String Tune;
 
     public HangDrum(String Tune) {
         this.Tune = Tune;
+    }
+
+    public String Play() {
+        return "Tuuuuuuummmm!";
     }
 }
