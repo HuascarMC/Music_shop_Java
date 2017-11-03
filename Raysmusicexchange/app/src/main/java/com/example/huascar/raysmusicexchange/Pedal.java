3d2bb5f6fd43fb66a027606f04d1f728e8b03d78
Huascar <huascarmc@hotmail.com>
2017-11-03 11:46:09 +0000

    pedal constructed

--- Raysmusicexchange/app/src/main/java/com/example/huascar/raysmusicexchange/Pedal.java
+++ Raysmusicexchange/app/src/main/java/com/example/huascar/raysmusicexchange/Pedal.java
@@ -4,5 +4,8 @@ package com.example.huascar.raysmusicexchange;
  * Created by huascar on 03/11/2017.
  */
 
-public class Pedal {
+public class Pedal extends Item {
+    public Pedal(String Type, double buyPrice, double sellPrice) {
+        super(Type, buyPrice, sellPrice);
+    }
 }
