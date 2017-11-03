310228b4a9eae5842451f77c8156633296569013
Huascar <huascarmc@hotmail.com>
2017-11-03 11:43:56 +0000

    guitarstrings constructed

---
+++ Raysmusicexchange/app/src/main/java/com/example/huascar/raysmusicexchange/GuitarStrings.java
@@ -0,0 +1,11 @@
+package com.example.huascar.raysmusicexchange;
+
+/**
+ * Created by huascar on 03/11/2017.
+ */
+
+public class GuitarStrings extends Item {
+    public GuitarStrings(String Type, double buyPrice, double sellPrice) {
+        super(Type, buyPrice, sellPrice);
+    }
+}
---
+++ Raysmusicexchange/app/src/main/java/com/example/huascar/raysmusicexchange/Pedal.java
@@ -0,0 +1,8 @@
+package com.example.huascar.raysmusicexchange;
+
+/**
+ * Created by huascar on 03/11/2017.
+ */
+
+public class Pedal {
+}
