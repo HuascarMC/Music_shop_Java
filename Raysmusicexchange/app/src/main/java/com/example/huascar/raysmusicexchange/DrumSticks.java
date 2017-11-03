96de4e3ba98aadba9177858cca713591afad940b
Huascar <huascarmc@hotmail.com>
2017-11-03 11:45:05 +0000

    drumsticks constructed

--- Raysmusicexchange/app/src/main/java/com/example/huascar/raysmusicexchange/DrumSticks.java
+++ Raysmusicexchange/app/src/main/java/com/example/huascar/raysmusicexchange/DrumSticks.java
@@ -4,5 +4,8 @@ package com.example.huascar.raysmusicexchange;
  * Created by huascar on 03/11/2017.
  */
 
-public class DrumSticks {
+public class DrumSticks extends Item {
+    public DrumSticks(String Type, double buyPrice, double sellPrice) {
+        super(Type, buyPrice, sellPrice);
+    }
 }
